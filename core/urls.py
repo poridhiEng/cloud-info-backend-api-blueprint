413476d5509d64e45a84197e59e95cdcc26c7555
0xh7ml <0xh7ml.py@gmail.com>
2025-06-26 13:09:40 +0600

    fix: Corrected URL paths for PricePlan, Machine, and Enrollment apps to include 'api/v1/'

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('PricePlan.urls')),
-    path('', include('Machine.urls')),
-    path('', include('Enrollment.urls')),
+    path('api/v1/', include('PricePlan.urls')),
+    path('api/v1/', include('Machine.urls')),
+    path('api/v1/', include('Enrollment.urls')),
 ]
